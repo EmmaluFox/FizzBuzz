171c6b327563b876867ce43e82318ae4f1b33ce1
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-03 16:07:21 +0000

    Not working

--- Program.cs
+++ Program.cs
@@ -4,35 +4,56 @@ namespace FizzBuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         PrintNums();
         }
 
         private static void PrintNums()
         {
+            string text = "";
             for (int i = 1; i < 101; i++)
             {
-                int fizzRemainder = i % 3;
-                int buzzRemainder = i % 5;
-                if (fizzRemainder + buzzRemainder == 0)
+                
+                int[] divisors = {3, 5, 7, 11, 13, 17};
+                
+                if (testRemainder == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
-                    
-                }
-                else if (fizzRemainder == 0)
-                {
-                    Console.WriteLine("Fizz");
-                } else if(buzzRemainder == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
+                    for (int ix = 0; ix < 6; ix++)
+                    {
+                        int positionCount = ix;
+                        int testRemainder = divisors[positionCount]; 
+                        
+                        string[] namesPrint = new string[6];
+                        namesPrint[0] = "Fizz";
+                        namesPrint[1] = "Buzz";
+                        namesPrint[2] = "Bang";
+                        namesPrint[3] = "Bong";
+                        namesPrint[4] = "Fezz";
+                        namesPrint[5] = "X";
 
-                    Console.WriteLine(i);
+                        string ConcatStr()
+                        {
+                            string nameTemp = "";
+                            if (i % divisors[ix] == 0)
+                            {
+                                string name = namesPrint[ix];
+                                nameTemp += name;
+                            }
+
+                            return nameTemp;
+                        }
+
+                        text += ConcatStr();
+                        Console.WriteLine(text);
+                    }
+                }
+                else {
+
+                        Console.WriteLine(i);
                 }
             }
         }
+        }
+
     }
-}
